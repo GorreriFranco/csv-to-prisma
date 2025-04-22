50687f04a0e0d55d4240eb3124f082a5091a2bdf
Franco Gorreri <franco.gorreri@gmail.com>
2025-04-22 16:58:11 -0300

    feat: added boilerplate to index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-// 
+import { readCSV } from "./csv-parser-utils";
+import { IceCream, PrismaClient } from "./generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const heladosOnDB = await prisma.iceCream.findMany();
+
+  console.log("Icecream on db:");
+  heladosOnDB.forEach((helado) => {
+    console.log(`- ${helado.flavor}: $${helado.price}`);
+  }
+  );
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
